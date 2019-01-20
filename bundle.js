d377e0d26c2e29aea7f5b21b251c1481c59081a2
Nilesh Gokhale <nileshgokhale45@gmail.com>
2019-01-19 16:31:45 -0800

    added bundlejs

---
+++ bundle.js
@@ -0,0 +1,116 @@
+( function(){
+  var _react = require('react');
+
+  var React = _interopRequireWildcard(_react);
+
+  var _reactNative = require('react-native');
+
+function _interopRequireWildcard(obj) {
+  if (obj && obj.__esModule) {
+    return obj;
+  } else {
+    var newObj = {};
+    if (obj != null) {
+      for (var key in obj) {
+        if (Object.prototype.hasOwnProperty.call(obj, key))
+        newObj[key] = obj[key];
+      }
+    }
+    newObj.default = obj; return newObj;
+  }
+}
+
+var App = function (_React$Component) {
+  _inherits(App, _React$Component);
+
+  function App(props) {
+    _classCallCheck(this, App);
+
+    var _this = _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).call(this, props));
+
+    _this.toggleDrawer = function () {
+      _this.setState({ showDrawer: !_this.state.showDrawer });
+    };
+
+    _this.state = {
+      showDrawer: false
+    };
+    _this.Navigation = createAppContainer(navigator);
+    return _this;
+  }
+
+  _createClass(App, [{
+    key: 'render',
+    value: function render() {
+      var _this2 = this;
+
+      var showDrawer = this.state.showDrawer ? React.createElement(Drawer, this.props) : null;
+      return React.createElement(
+        _reactNative.View,
+        null,
+        React.createElement(
+          _reactNative.View,
+          null,
+          React.createElement(
+            _reactNative.TouchableHighlight,
+            { style: { marginTop: 10, borderRadius: 20, borderColor: 'red' }, onPress: function onPress() {
+                return _this2.toggleDrawer();
+              } },
+            React.createElement(_reactNative.Image, { style: { width: 50, height: 50, marginTop: 20 }, source: { uri: 'https://icons-for-free.com/free-icons/png/64/105234.png' } })
+          )
+        ),
+        React.createElement(
+          _reactNative.View,
+          null,
+          showDrawer
+        )
+      );
+    }
+  }]);
+
+  return App;
+}(React.Component);
+
+exports.default = App;
+
+var Drawer = function (_React$Component2) {
+  _inherits(Drawer, _React$Component2);
+
+  function Drawer(props) {
+    _classCallCheck(this, Drawer);
+
+    var _this3 = _possibleConstructorReturn(this, (Drawer.__proto__ || Object.getPrototypeOf(Drawer)).call(this, props));
+
+    var ds = new _reactNative.ListView.DataSource({ rowHasChanged: function rowHasChanged(r1, r2) {
+        return r1 !== r2;
+      } });
+    _this3.state = {
+      dataSource: ds.cloneWithRows(['row1', 'row 2', 'row 3'])
+    };
+    return _this3;
+  }
+
+  _createClass(Drawer, [{
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        _reactNative.View,
+        null,
+        React.createElement(_reactNative.ListView, {
+          dataSource: this.state.dataSource,
+          renderRow: function renderRow(rowData) {
+            return React.createElement(
+              _reactNative.Text,
+              null,
+              rowData
+            );
+          }
+        })
+      );
+    }
+  }]);
+
+  return Drawer;
+}(React.Component);
+
+})
