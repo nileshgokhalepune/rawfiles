9f2551485dbf289956a208ab0368f632ed957e2c
Nilesh Gokhale <nileshgokhale45@gmail.com>
2019-01-20 11:53:54 -0800

    removed require dependency

--- bundle.js
+++ bundle.js
@@ -1,24 +1,4 @@
-( function(){
-  var _react = require('react');
-
-  var React = _interopRequireWildcard(_react);
-
-  var _reactNative = require('react-native');
-
-function _interopRequireWildcard(obj) {
-  if (obj && obj.__esModule) {
-    return obj;
-  } else {
-    var newObj = {};
-    if (obj != null) {
-      for (var key in obj) {
-        if (Object.prototype.hasOwnProperty.call(obj, key))
-        newObj[key] = obj[key];
-      }
-    }
-    newObj.default = obj; return newObj;
-  }
-}
+( function(React, _reactNative){
 
 var App = function (_React$Component) {
   _inherits(App, _React$Component);
