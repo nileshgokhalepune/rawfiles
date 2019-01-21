0d637ce9e145ce5bdff908b3b4c84dea4245eeb5
Nilesh Gokhale <nileshgokhale45@gmail.com>
2019-01-20 22:04:23 -0800

    changed the structure

--- bundle.js
+++ bundle.js
@@ -1,9 +1,7 @@
-(function(React, _reactNative, createStackNavigator, createAppContainer){
+(function(React, _reactNative, _reactNavigation){
 
-
-
-  var Home = exports.Home = function (_React$Component3) {
-    _inherits(Home, _React$Component3);
+  var Home = exports.Home = function (_React$Component) {
+    _inherits(Home, _React$Component);
 
     function Home() {
       _classCallCheck(this, Home);
@@ -25,8 +23,8 @@
     return Home;
   }(React.Component);
 
-  var Other = exports.Other = function (_React$Component4) {
-    _inherits(Other, _React$Component4);
+  var Other = exports.Other = function (_React$Component2) {
+    _inherits(Other, _React$Component2);
 
     function Other() {
       _classCallCheck(this, Other);
@@ -48,103 +46,104 @@
     return Other;
   }(React.Component);
 
-  const navigator = createStackNavigator({
-    Home : {screen: Home },
-    Other : {screen: Other}
-  })
+  var navigator = (0, _reactNavigation.createStackNavigator)({
+    Home: { screen: Home },
+    Other: { screen: Other }
+  });
 
-var App = function (_React$Component) {
-  _inherits(App, _React$Component);
+  var App = function (_React$Component3) {
+    _inherits(App, _React$Component3);
 
-  function App(props) {
-    _classCallCheck(this, App);
+    function App(props) {
+      _classCallCheck(this, App);
 
-    var _this = _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).call(this, props));
+      var _this3 = _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).call(this, props));
 
-    _this.toggleDrawer = function () {
-      _this.setState({ showDrawer: !_this.state.showDrawer });
-    };
+      _this3.toggleDrawer = function () {
+        _this3.setState({ showDrawer: !_this3.state.showDrawer });
+      };
 
-    _this.state = {
-      showDrawer: false
-    };
-    _this.Navigation = createAppContainer(navigator);
-    return _this;
-  }
+      _this3.state = {
+        showDrawer: false
+      };
+      _this3.Navigation = (0, _reactNavigation.createAppContainer)(navigator);
+      return _this3;
+    }
 
-  _createClass(App, [{
-    key: 'render',
-    value: function render() {
-      var _this2 = this;
+    _createClass(App, [{
+      key: 'render',
+      value: function render() {
+        var _this4 = this;
 
-      var showDrawer = this.state.showDrawer ? React.createElement(Drawer, this.props) : null;
-      return React.createElement(
-        _reactNative.View,
-        null,
-        React.createElement(
+        var showDrawer = this.state.showDrawer ? React.createElement(Drawer, this.props) : null;
+        return React.createElement(
           _reactNative.View,
           null,
           React.createElement(
-            _reactNative.TouchableHighlight,
-            { style: { marginTop: 10, borderRadius: 20, borderColor: 'red' }, onPress: function onPress() {
-                return _this2.toggleDrawer();
-              } },
-            React.createElement(_reactNative.Image, { style: { width: 50, height: 50, marginTop: 20 }, source: { uri: 'https://icons-for-free.com/free-icons/png/64/105234.png' } })
+            _reactNative.View,
+            null,
+            React.createElement(
+              _reactNative.TouchableHighlight,
+              { style: { marginTop: 10, borderRadius: 20, borderColor: 'red' }, onPress: function onPress() {
+                  return _this4.toggleDrawer();
+                } },
+              React.createElement(_reactNative.Image, { style: { width: 50, height: 50, marginTop: 20 }, source: { uri: 'https://icons-for-free.com/free-icons/png/64/105234.png' } })
+            )
+          ),
+          React.createElement(
+            _reactNative.View,
+            null,
+            showDrawer
           )
-        ),
-        React.createElement(
+        );
+      }
+    }]);
+
+    return App;
+  }(React.Component);
+
+  exports.default = App;
+
+  var Drawer = function (_React$Component4) {
+    _inherits(Drawer, _React$Component4);
+
+    function Drawer(props) {
+      _classCallCheck(this, Drawer);
+
+      var _this5 = _possibleConstructorReturn(this, (Drawer.__proto__ || Object.getPrototypeOf(Drawer)).call(this, props));
+
+      var ds = new _reactNative.ListView.DataSource({ rowHasChanged: function rowHasChanged(r1, r2) {
+          return r1 !== r2;
+        } });
+      _this5.state = {
+        dataSource: ds.cloneWithRows(['row1', 'row 2', 'row 3'])
+      };
+      return _this5;
+    }
+
+    _createClass(Drawer, [{
+      key: 'render',
+      value: function render() {
+        return React.createElement(
           _reactNative.View,
           null,
-          showDrawer
-        )
-      );
-    }
-  }]);
+          React.createElement(_reactNative.ListView, {
+            dataSource: this.state.dataSource,
+            renderRow: function renderRow(rowData) {
+              return React.createElement(
+                _reactNative.Text,
+                null,
+                rowData
+              );
+            }
+          })
+        );
+      }
+    }]);
 
-  return App;
-}(React.Component);
+    return Drawer;
+  }(React.Component);
 
-exports.default = App;
-
-var Drawer = function (_React$Component2) {
-  _inherits(Drawer, _React$Component2);
-
-  function Drawer(props) {
-    _classCallCheck(this, Drawer);
-
-    var _this3 = _possibleConstructorReturn(this, (Drawer.__proto__ || Object.getPrototypeOf(Drawer)).call(this, props));
-
-    var ds = new _reactNative.ListView.DataSource({ rowHasChanged: function rowHasChanged(r1, r2) {
-        return r1 !== r2;
-      } });
-    _this3.state = {
-      dataSource: ds.cloneWithRows(['row1', 'row 2', 'row 3'])
-    };
-    return _this3;
-  }
-
-  _createClass(Drawer, [{
-    key: 'render',
-    value: function render() {
-      return React.createElement(
-        _reactNative.View,
-        null,
-        React.createElement(_reactNative.ListView, {
-          dataSource: this.state.dataSource,
-          renderRow: function renderRow(rowData) {
-            return React.createElement(
-              _reactNative.Text,
-              null,
-              rowData
-            );
-          }
-        })
-      );
-    }
-  }]);
-
-  return Drawer;
-}(React.Component);
 
   return {
     App: App
