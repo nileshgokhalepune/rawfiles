ed6f5f1a442c38a526f3920b4250421c0e04dd10
Nilesh Gokhale <nileshgokhale45@gmail.com>
2019-01-24 08:44:29 -0800

    minor change:

--- bundle.js
+++ bundle.js
@@ -32,7 +32,7 @@ var AccountSummary = exports.AccountSummary = function (_React$Component2) {
     var _this2 = _possibleConstructorReturn(this, (AccountSummary.__proto__ || Object.getPrototypeOf(AccountSummary)).call(this, props));
 
     _this2.goBack = function () {
-      _this2.props.navigate('DrawerOpen');
+      _this2.props.navigation.navigate('DrawerOpen');
           _this2.props.navigation.navigate('App');
         };
 
