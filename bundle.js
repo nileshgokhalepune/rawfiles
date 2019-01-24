c8e2ccd0e420ecedebf6ccfce7a89a62f2c1b4d5
Nilesh Gokhale <nileshgokhale45@gmail.com>
2019-01-23 21:30:57 -0800

    change acc summ

--- bundle.js
+++ bundle.js
@@ -31,7 +31,7 @@ var AccountSummary = exports.AccountSummary = function (_React$Component2) {
 
     var _this2 = _possibleConstructorReturn(this, (AccountSummary.__proto__ || Object.getPrototypeOf(AccountSummary)).call(this, props));
 
-    _this3.goBack = function () {
+    _this2.goBack = function () {
           _this3.props.navigation.navigateTo('App');
         };
 
