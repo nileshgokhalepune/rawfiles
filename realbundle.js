8f9cde4ed229452be5003b31a53360f6d995041d
nileshgokhale <nileshgokhale45@gmail.com>
2019-01-28 09:37:03 -0800

    added token

--- realbundle.js
+++ realbundle.js
@@ -1,5 +1,34 @@
-(function main(React, _reactNative, _nativebase, reactNavigation, styles, createAppContainer, createDrawerNavigator, apiBase) {
+(function main(React, _reactNative, _nativebase, reactNavigation, styles, createAppContainer, createDrawerNavigator, apiBase, AsyncStorage) {
 	'use strict';
+
+	function _asyncToGenerator(fn) {
+		return function () {
+			var gen = fn.apply(this, arguments);
+			return new Promise(function (resolve, reject) {
+				function step(key, arg) {
+					try {
+						var info = gen[key](arg);
+						var value = info.value;
+					} catch (error) {
+						reject(error);
+						return;
+					}
+					if (info.done) {
+						resolve(value);
+					} else {
+						return Promise.resolve(value).then(
+							function (value) {
+							step("next", value);
+						},
+							function (err) {
+							step("throw", err);
+						});
+					}
+				}
+				return step("next");
+			});
+		};
+	}
 
 	var LoginModule = (exports.LoginModule = (function (_React$Component) {
 			_inherits(LoginModule, _React$Component);
@@ -75,6 +104,47 @@
 						_this.login();
 					}
 				};
+
+				_this.storeData = (function () {
+					var _ref = _asyncToGenerator(
+							/*#__PURE__*/
+							regeneratorRuntime.mark(function _callee(token) {
+								return regeneratorRuntime.wrap(
+									function _callee$(_context) {
+									while (1) {
+										switch ((_context.prev = _context.next)) {
+										case 0:
+											_context.prev = 0;
+											_context.next = 3;
+											return _reactNative.AsyncStorage.setItem(
+												"X_CSRF_TOKEN",
+												token);
+
+										case 3:
+											_context.next = 8;
+											break;
+
+										case 5:
+											_context.prev = 5;
+											_context.t0 = _context["catch"](0);
+
+											console.log("error saving data");
+
+										case 8:
+										case "end":
+											return _context.stop();
+										}
+									}
+								},
+									_callee,
+									_this2,
+									[[0, 5]]);
+							}));
+
+					return function (_x) {
+						return _ref.apply(this, arguments);
+					};
+				})();
 
 				_this.state = {};
 				return _this;
@@ -158,6 +228,39 @@
 							Object.getPrototypeOf(AccountSummaryModule)).call(this, props));
 				_this2.state = {};
 				_this2.shouldRenderModule = false;
+				_this2.getToken = _asyncToGenerator(
+						/*#__PURE__*/
+						regeneratorRuntime.mark(function _callee2() {
+							var value;
+							return regeneratorRuntime.wrap(
+								function _callee2$(_context2) {
+								while (1) {
+									switch ((_context2.prev = _context2.next)) {
+									case 0:
+										_context2.prev = 0;
+										_context2.next = 3;
+										return _reactNative.AsyncStorage.getItem("X_CSRF_TOKEN");
+
+									case 3:
+										value = _context2.sent;
+										return _context2.abrupt("return", value);
+
+									case 7:
+										_context2.prev = 7;
+										_context2.t0 = _context2["catch"](0);
+
+										console.log("error getting data");
+
+									case 10:
+									case "end":
+										return _context2.stop();
+									}
+								}
+							},
+								_callee2,
+								_this5,
+								[[0, 7]]);
+						}));
 
 				_this2.resetSearch = function () {};
 
@@ -215,7 +318,7 @@
 							})
 							.then(function (data) {
 								var headers = {
-									"X-CSRF-TOKEN": "",
+									"X-CSRF-TOKEN":  _this4.getToken(),
 									"X-Request-Token": data.getAccountsAt.token
 								};
 								fetch(data.getAccountsAt.url, {
